a47b0168fe92e87339de1a5445417980dbd9a53b
nickb <nickb>
2004-12-13 22:19:32 +0000

    delete

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<p>
-	<a href="http://www.eclipse.org/emf" target="_top">EMF Home</a> |
-	<a href="http://www.eclipse.org/xsd" target="_top">XSD Home</a> | 
-	<a href="#top">Top of Page</a>
-</p>
-
-</body></html>
