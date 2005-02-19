3276bdf9a239cda6994ca60eeecd322f9b5ce824
nickb <nickb>
2005-02-19 07:30:00 +0000

    create new plan document

---
+++ plan/plan.php
@@ -0,0 +1,325 @@
+<?php /* given data from bugzilla, generate a report of the open bugs and their prioritization / estimated time to complete / comments
+
+get this to collect meta:
+https://bugs.eclipse.org/bugs/colchange.cgi?rememberedquery=product%3DEMF%2CXSD%26bug_status%3DNEW%26bug_status%3DASSIGNED%26bug_status%3DREOPENED%26order%3Dbugs.bug_status%2Cbugs.target_milestone%2Cbugs.bug_id%26query_format%3Dadvanced&column_product=on&column_component=on&column_version=on&column_target_milestone=on&column_opendate=on&column_reporter=on&column_rep_platform=on&column_assigned_to=on&column_votes=on&column_changeddate=on&column_resolution=on&column_bug_severity=on&column_priority=on&column_bug_status=on&column_short_desc=on&splitheader=0
+
+which should repost to here:
+https://bugs.eclipse.org/bugs/buglist.cgi?product=EMF,XSD&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&order=bugs.bug_status,bugs.target_milestone,bugs.bug_id&query_format=advanced
+
+and to get buglist:
+
+	<input type="hidden" name="buglist" value="43752,51210,61251,61639,61751,68396,72215,73143,74945,75617,75756,75921,75922,75923,75925,75929,75931,75933,76538,77518,78076,79089,79670,79768,80796,80806,81752,82597,82624,82626,82809,82899,83055,83223,83463,83591,83612,83714,83873,83876,83899,84119,84327,84563,84564,84813,84950,85852,82613,70862,82611,82615,82610,82614,85202">
+
+then get() this:
+
+https://bugs.eclipse.org/bugs/long_list.cgi?buglist=43752,51210,61251,61639,61751,68396,72215,73143,74945,75617,75756,75921,75922,75923,75925,75929,75931,75933,76538,77518,78076,79089,79670,79768,80796,80806,81752,82597,82624,82626,82809,82899,83055,83223,83463,83591,83612,83714,83873,83876,83899,84119,84327,84563,84564,84813,84950,85852,82613,70862,82611,82615,82610,82614,85202
+
+to parse out [plan] values and fields missing from above (cuz curl() loses cookied values that bugzilla needs to display the correct columns)
+*/
+
+$contentType = $_GET["Content-Type"] ? $_GET["Content-Type"] : "text/xml"; // default or qs value
+
+$usetmpfile=0; // defines whether or not to use pregen'd tmp file (1) or else to regen on the fly (0)
+
+$bugz = array(); // $bugz[$num] = array(field => value, field2 => value2, ...);
+$buglist = ""; // "75933,76538,77518,78076,79089, ... ";
+$columns = array(
+	"ID" => "ID",
+	"Product" => "Product",
+	"Component" => "Comp",
+	"Version" => "Ver",
+	"Target Milestone" => "TargetM",
+
+	"Opened" => "Opened",
+	"Reported By" => "Reporter",
+	"Assigned To" => "Assignee",
+
+	//"Votes" => "Votes",
+	//"Changed Date" => "Changed",
+	//"Resolution" => "Resolution",
+
+	"Severity" => "Sev",
+	"Priority" => "Pri",
+	"Status" => "Stat",
+
+	"Summary" => "Summary"
+);
+$additional_columns = array(
+	"Plan Priority"=>"Plan-Priority",
+	"Plan Estimate"=>"Plan-Estimate",
+	"Plan Comments"=>"Plan-Comments"
+);
+
+$column_order = array( // column label => field name (mixed case version)
+	"ID" => "ID",
+	"Prod" => "Product",
+	"Comp" => "Comp",
+	"Ver" => "Ver",
+	"TargetM" => "TargetM",
+
+	"Opened" => "Opened",
+	"Reported By" => "Reporter",
+	"Assigned To" => "Assignee",
+
+	"Sev" => "Sev",
+	"Pri" => "Pri",
+	"Stat" => "Stat",
+
+	"Plan Priority"=>"Plan-Priority",
+	"Plan Estim"=>"Plan-Estimate",
+	"Plan Comments"=>"Plan-Comments",
+
+	"Summary" => "Summary"
+);
+
+getMetaAndBugList();
+getPlanItems($columns); 
+if ($contentType=="text/html") { 
+	wArr($bugz);
+} else if ($contentType=="text/xml") { 
+	displayXML();
+}
+
+/************************************** FUNCTIONS ***********************************/
+
+function getMetaAndBugList() {
+	global $bugz,$buglist,$usetmpfile;
+
+	if (!$usetmpfile) { 
+		$html = https_file("https://bugs.eclipse.org/bugs/buglist.cgi?product=EMF,XSD&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&order=bugs.bug_status,bugs.target_milestone,bugs.bug_id&query_format=advanced"); // wArr($html);
+		$fh = fopen("/tmp/out.html","w");
+		foreach ($html as $line) { 
+			fputs($fh,$line."\n");
+		}
+		fclose($fh);
+	} else {
+		$html = file("/tmp/out.html");
+	}
+	//w(sizeof($html),1);
+
+	$loading=0;
+	$bugnum="";
+	//echo "<pre>";
+	// foreach ($html as $line) { echo $line; }
+	$cols = array(
+		"Severity", "Priority", "Platform", "Assignee", "Status", "Resolution", "Summary"
+	);
+	foreach ($html as $line) { 
+		// look for the following values: ID, Sev, Pri, Plt, Assignee, Status, Resolution, Summary
+		if (isIn($line,"show_bug.cgi?id=")) { // link to bug means start of a bug entry row
+			$loading=1;
+			if (preg_match("/id=(\d+)/",$line,$m)) { 
+				$bugnum=$m[1];
+				$bugz[$bugnum] = array("ID" => $bugnum);
+			} else {
+				$bugnum="n/a";
+				$bugz[$bugnum] = array("ID" => $bugnum);
+			}
+		} else if (isIn($line,"</tr>")) { // end of a row
+			$loading=0;
+			$col=0; // reset column pointer
+		} else if ($loading && isIn($line,"<td>")) { 
+			//w($line);
+			$bugz[$bugnum][$cols[$col]] = trim(str_replace("<td>","",str_replace("<nobr>","",str_replace("</nobr>","",$line))));
+			//w("\$bugz[".$bugnum."][".$cols[$col]."] = ".$bugz[$bugnum][$cols[$col]],"\n");
+			$col++; // increment to next column pointer
+		} else if (isIn($line,"name=\"buglist\"")) { // also list of bugs
+			//w("<pre>$line</pre>");
+			$buglist=str_replace("\">","",str_replace("<input type=\"hidden\" name=\"buglist\" value=\"","",trim($line)));
+			//w($buglist);
+		}
+	}
+	//echo "</pre>";
+
+}
+
+function getPlanItems($extrafields=array()) {
+	global $bugz,$buglist,$usetmpfile;
+
+	if (!$usetmpfile) { 
+		$html = https_file("https://bugs.eclipse.org/bugs/long_list.cgi?buglist=$buglist"); // wArr($html);
+		$fh = fopen("/tmp/out2.html","w");
+		foreach ($html as $line) { 
+			fputs($fh,$line."\n");
+		}
+		fclose($fh);
+	} else {
+		$html = file("/tmp/out2.html");
+	}
+	//w(sizeof($html),1);
+
+	$loading=0;
+	foreach ($html as $line) { 
+		if (isIn($line,"<font size=\"+3\">Bug ")) { // name of bug means start of a bug 
+			if (preg_match("/\<font size\=\"\+3\"\>Bug\ (\d+)/",$line,$m)) { 
+				$bugnum=$m[1];
+			} else {
+				$bugnum="n/a";
+			}
+			//w("<b>BUG: $bugnum</b>",1);
+		} else if (sizeof($extrafields)>0 && isIn($line,"<b>") && isIn($line,"</b>&nbsp;")) {
+			//w($line,1);
+			if (preg_match("/\<b\>(.+)\:\<\/b\>\&nbsp\;(.+)/",$line,$m)) {
+				$key = $m[1];
+				$val = $m[2];
+				//w("<i>$key = $val</i>",1);
+				if (array_key_exists($key,$extrafields)) {
+					$bugz[$bugnum][$extrafields[$key]] = trim($val);
+					//w("\$bugz[$bugnum][".$extrafields[$key]."] = ".htmlentities($bugz[$bugnum][$extrafields[$key]]),1);
+				} else {
+					//w("key $key not collected.",1);
+				}
+			}
+		} else if (isIn($line,"[plan")) { // get plan items
+			//w($line,1);
+			if (!isIn($line,"/]")) { 
+				$loading=1;
+			}
+			if (preg_match("/pri\=(\d)/",$line,$m)) { // [plan pri=2 est=2w/]
+				$bugz[$bugnum]["Plan-Priority"] = $m[1];
+			}
+			if (preg_match("/est=(\d[dwm])/",$line,$m)) { // [plan pri=2 est=2w/]
+				$bugz[$bugnum]["Plan-Estimate"] = $m[1];
+			}
+			if (preg_match("/](.+)\[\/plan\]/",$line,$m)) { // [plan pri=2 est=1d]This is mostly done already.[/plan]
+				$bugz[$bugnum]["Plan-Comments"] = $m[1];
+				$loading=0;
+			} else if (preg_match("/](.+)/",$line,$m)) { // [plan pri=2 est=1d]This is mostly done already.[/plan]
+				$bugz[$bugnum]["Plan-Comments"] = $m[1];
+			}
+			$ret = trimTrail($bugz[$bugnum]["Plan-Comments"],$loading); $bugz[$bugnum]["Plan-Comments"] = $ret[0]; $loading = $ret[1]; $ret = "";
+
+			//w("\$bugz[$bugnum][Plan Estimate] = ".$bugz[$bugnum]["Plan Estimate"],1);
+			//w("\$bugz[$bugnum][Plan Priority] = ".$bugz[$bugnum]["Plan Priority"],1);
+			//w("\$bugz[$bugnum][Plan-Comments] = ".htmlentities($bugz[$bugnum]["Plan-Comments"]),1);
+		} else if ($loading) { // collect more plan item content (if necessary)
+			//w($line,1);
+			if (preg_match("/(.+)\[\/plan\]/",$line,$m)) { // [plan pri=2 est=1d]This is mostly done already.[/plan]
+				$bugz[$bugnum]["Plan-Comments"] .= " ".$m[1];
+				$loading=0;
+			} else { 
+				$bugz[$bugnum]["Plan-Comments"] .= " ".$line;
+			}
+			$ret = trimTrail($bugz[$bugnum]["Plan-Comments"],$loading); $bugz[$bugnum]["Plan-Comments"] = $ret[0]; $loading = $ret[1]; $ret = "";
+			//w("\$bugz[$bugnum][Plan-Comments] = ".htmlentities($bugz[$bugnum]["Plan-Comments"]),1);
+		}
+	}
+	//wArr($bugz);
+
+}
+
+function displayXML() {
+	global $bugz,$columns,$additional_columns,$column_order;
+	header('Content-type: text/xml');
+	echo '<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="plan.xsl"?>
+<!-- $Id: plan.php,v 1.1 2005/02/19 07:30:00 nickb Exp $ -->
+<plan>
+	<modified>$Date: 2005/02/19 07:30:00 $</modified>
+
+	<product-def product="EMF" label="EMF Development Plan" />
+	<product-def product="XSD" label="XSD Development Plan" />
+
+'; ?><?php
+	foreach ($column_order as $label => $col) {
+		if (in_array($col,$columns)) {
+			echo "\t\t<column-def column=\"".strtolower($col)."\" label=\"".$label."\"/>\n";
+		} else if (in_array($col,$additional_columns)) {
+			echo "\t\t<column-def column=\"".strtolower($col)."\" label=\"".$label."\"/>\n";
+		}
+	}
+	echo "\n";
+	foreach ($bugz as $bugnum => $data) { 
+		echo "\t<bug>\n";
+		foreach ($column_order as $col) {
+			if (in_array($col,$columns)) {
+				echo "\t\t<".strtolower($col).">".$data[$col]."</".strtolower($col).">\n";
+			} else if (in_array($col,$additional_columns)) {
+				echo "\t\t<".strtolower($col).">".$data[$col]."</".strtolower($col).">\n";
+			}
+		}
+		echo "\t</bug>\n";
+	}
+	echo '</plan>
+';
+}
+
+function trimTrail($in,$loading) {
+	if (isIn($in,"[/plan]")) { // trim off at this point, set loading to 0
+		$in = substr($in,0,strpos($in,"[/plan]"));
+		$loading=0;
+	}
+	if (isIn($in,"</pre>")) { // trim off at this point, set loading to 0
+		$in = substr($in,0,strpos($in,"</pre>"));
+	}
+	return array($in,$loading);
+}
+
+//*** usual stuff ***//
+
+	function isIn($haystack,$needle) {
+		return (false!==strpos($haystack,$needle));
+	}
+
+	function https_file($url) {
+		ini_set("display_errors","0"); // suppress file not found errors
+		// $html = file("https://..."); // only works in php 4.3.1 and later
+		// new for PHP 4.1.2 with libcurl on emf.torolab.ibm.com
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$text = curl_exec($ch);
+		curl_close($ch);
+		$html = preg_split("/\n/",$text);
+		ini_set("display_errors","1"); // and turn 'em back on.
+		return $html;
+	}
+
+	function wArr($arr) { // ie., wArr(array,separator,showKeys,trailingCharacter);
+	// since PHP won't display an array's contents when you do echo($array), this returns the array like this:
+	/* usage: wArr($array)		// 0:apple, 1:peach, 2:grapes\n
+			  wArr($array,"\n") (use "\n" as separator instead of ", ")
+			  wArr($array,"",false); (use default separator, but don't display keys 			
+			  wArr($array,"",false,false); (use default separator, but don't display keys and no trailing char	*/
+		$sep=(func_num_args()>1&&func_get_arg(1))?func_get_arg(1):"<br>"; 
+		$key=(func_num_args()>2)?func_get_arg(2):true;  // assume we want keys
+		$trail=(func_num_args()>3)?func_get_arg(3):"\n";  // assume we want a trailing newline
+		$i=0;
+		if (is_array($arr) && sizeof($arr)>0) { 
+			foreach ($arr as $ark => $arv) {
+				w(($key?$ark.": ":"")); 
+				if (is_array($arv)) { 
+					w("<ul>");
+					wArr($arv,$sep,$key,$trail);
+					w("</ul>");
+				} else {
+					w($arv);
+				}
+				$i++;
+				if ($i<sizeof($arr)) { w($sep); }
+			} 	
+			w($trail);
+		} else {
+			//w($arr.$trail);
+		}
+	} 
+
+	function w($s) { // shortcut for echo() with second parameter: "add break+newline"
+		if (func_num_args()<=1) { 
+			$br=""; // no break/newline
+		} else { 
+			$br=func_get_arg(1);
+			if (stristr($br,"b")) {
+				$br="<br>";
+			} else if (stristr($br,"n")) {
+				$br="\n";
+			} else if ($br) { 
+				$br="<br>\n"; 
+			}
+		}
+		echo($s.$br); 
+	}
+
+
+
+?>
---
+++ plan/plan.xsl
@@ -0,0 +1,282 @@
+<xsl:stylesheet version = '1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform' xmlns:msxsl="urn:schemas-microsoft-com:xslt">
+<xsl:output method="html" encoding="ISO-8859-1"/>
+
+	<xsl:key name="bugEntry" match="bug" use="product"/>
+
+	<xsl:param name="showFiltersOrHeaderFooter"></xsl:param> <!-- LEAVE BLANK - pass value of '1' into stylesheet via javascript -->
+	<xsl:param name="product"></xsl:param> 
+	<xsl:param name="version"></xsl:param>
+
+<xsl:variable name="xx">
+  <xsl:call-template name="show_plan_items">
+  </xsl:call-template>
+</xsl:variable>
+
+<xsl:template name="show_plan_items" match="/">
+<xsl:for-each select="plan">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+    <title>Eclipse Tools - EMF, SDO and XSD - Development Plan</title>
+    <link REL="SHORTCUT ICON" HREF="http://dev.eclipse.org/viewcvs/indextools.cgi/%7Echeckout%7E/emf-home/images/eclipse-icons/eclipse32.ico"/>
+	<script type="text/javascript" src="http://dev.eclipse.org/viewcvs/indextools.cgi/%7Echeckout%7E/emf-home/scripts/includes/nav.js"></script>
+	<link rel="stylesheet" href="http://dev.eclipse.org/viewcvs/indextools.cgi/%7Echeckout%7E/emf-home/scripts/includes/style.css" type="text/css"/>
+	<style>@import url("plan.css");</style>
+	</head>
+	<body>
+
+	<xsl:if test="$showFiltersOrHeaderFooter!='1'">
+	<!-- wrapper for left nav -->
+	<table cellspacing="0" cellpadding="0" border="0" width="100%">
+		<tr valign="top"><td colspan="1" align="left" width="100%"><table border="0" cellspacing="0" cellpadding="0" width="100%" BGCOLOR="#006699" >
+
+		 <tr>
+			  <td BGCOLOR="#000000" width="116" height="50"><a name="top"></a><a href="http://www.eclipse.org" target="_top"><img src="http://www.eclipse.org/images/EclipseBannerPic.jpg" width="115" height="50" border="0"/></a></td>
+			  <td width="637" height="50" style="background-repeat: repeat-y;" background="http://www.eclipse.org/images/gradient.jpg"></td>
+			  <td width="250" height="50"><img src="http://www.eclipse.org/images/eproduct-simple.GIF" width="250" height="48"/></td>
+		 </tr>
+
+		</table></td>
+	  </tr>
+	</table>
+	</xsl:if>
+
+<table cellspacing="0" cellpadding="0" border="0" width="100%">
+	<tr valign="top">
+		<td align="left" width="115" bgcolor="#6699CC">
+
+			<!-- left nav here -->
+			<xsl:if test="$showFiltersOrHeaderFooter!='1'">
+				<xsl:copy-of select="document('../includes/nav.xml')/div"/>
+			</xsl:if>
+
+		</td>
+
+		<td><img src="http://www.eclipse.org/images/c.gif" height="1" width="3"/></td><td align="left" width="100%">
+&#160;
+<table border="0" cellpadding="2" width="100%">
+  <tbody>
+
+    <tr>
+      <td align="left" width="60%">
+        <font class="indextop">
+		Development Plan<xsl:if test="$product!='' or $version!=''">:
+			<xsl:if test="$product!=''"><xsl:value-of select="$product" />&#160;</xsl:if>
+			<xsl:if test="$version!=''"><xsl:value-of select="$version" />&#160;</xsl:if>
+		</xsl:if>
+		</font><br/>
+        <font class="indexsub">Eclipse Modeling Framework</font>
+
+      </td>
+      <td width="40%">
+			<img src="http://dev.eclipse.org/viewcvs/indextools.cgi/%7Echeckout%7E/emf-home/images/reference.gif" hspace="50" align="right"/>
+      </td>
+
+    </tr>
+  </tbody>            
+</table>
+
+<table border="0" cellpadding="2" width="100%" >
+<tr>
+
+<td align="LEFT" valign="TOP" BGCOLOR="#0070A0"><b><font face="Arial,Helvetica"><font color="#FFFFFF">
+	Development Plan<xsl:if test="$product!='' or $version!=''">:
+		<xsl:if test="$product!=''"><xsl:value-of select="$product" />&#160;</xsl:if>
+		<xsl:if test="$version!=''"><xsl:value-of select="$version" />&#160;</xsl:if>
+	</xsl:if>
+</font></font></b><a name="top">&#160;</a></td>
+</tr>
+</table>
+<table border="0" cellpadding="2" width="100%" >
+<tr>
+<td align="right" valign="TOP"><b><small><a href="#quicknav">Quick Nav</a></small></b></td>
+</tr>
+<tr>
+<td>
+	<b class="big-header">Last modified: 
+	<xsl:value-of select="substring-before(substring-after(modified,concat('$','Date',':')),'$')"/>
+	</b>
+	<p>
+	<table><tr><td>Development Plan Summary:</td><td><a href="#EMF_dev">EMF</a> :: <a href="#XSD_dev">XSD</a></td></tr>
+	<tr><td>Development Plan Details:</td><td><a href="#EMF">EMF</a> :: <a href="#XSD">XSD</a></td></tr></table>
+	</p>
+	<p>Plan Priorities are on a scale from 1 to 4 where 1 is most prioritized, 4 is least prioritized.<br/>
+	Plan Estimates are annotated in units of days (d), weeks (w), or months (m), where 1m = 4w = 20d.<br/>
+	This Plan is subject to change.
+	</p>
+	<p>&#160;</p>
+
+</td>
+</tr>
+</table>
+
+	<!-- form controls -->
+<!--	<form action="plan.php" method="get" name="mainform">
+	<table width="60%">
+			<tr>
+				<td>
+					
+				</td>
+			</tr>
+	</table>
+	</form> -->
+	
+	<!-- nav header table -->
+	<table border="0" cellspacing="1" cellpadding="5" width="100%">
+	<xsl:for-each select="product-def">
+		<xsl:if test="((count(key('bugEntry',@product)) != 0 and $product = product) or $product = '')">
+			<tr class="header">
+				<td colspan="9" class="sub-header">
+					<a name="{@product}_dev"></a><xsl:value-of select="@label"/>: Summary (<xsl:value-of select="count(key('bugEntry',@product))" /> Bugs) - <a class="sub-header" href="#{@product}">see details</a>
+				</td>
+			</tr>
+			<tr class="content-header">
+				<xsl:for-each select="//column-def">
+					<xsl:if test="@column != 'product' and @column != 'reporter' and @column != 'assignee' and @column != 'pri' and @column != 'stat' and @column != 'plan-comments'">
+						<td colspan="1" class="sub-header">
+							<xsl:value-of select="@label" />
+						</td>
+					</xsl:if>
+				</xsl:for-each>
+			</tr>
+			<xsl:for-each select="key('bugEntry',@product)">
+				<xsl:sort select="plan-priority" data-type="text" order="ascending" />
+				<xsl:if test="(starts-with(version,$version) or $version = '') and ($product = product or $product = '')">
+					<tr valign="top">
+						<xsl:choose>
+						<xsl:when test="(position() mod 2 = 1)">
+							<xsl:attribute name="class">dark-row2</xsl:attribute>
+						</xsl:when>
+						<xsl:otherwise>
+							<xsl:attribute name="class">light-row</xsl:attribute>
+						</xsl:otherwise>
+						</xsl:choose>
+						<xsl:for-each select="./*">
+							<xsl:if test="name() != 'product' and name() != 'reporter' and name() != 'assignee' and name() != 'pri' and name() != 'stat' and name() != 'plan-comments'">
+								<td>
+								<xsl:choose>
+									<xsl:when test="name() = 'sev' and substring(../sev,1,3) = 'enh'">
+										<xsl:attribute name="class">italic</xsl:attribute>
+									</xsl:when>
+									<xsl:otherwise><xsl:attribute name="class">normal</xsl:attribute></xsl:otherwise>
+								</xsl:choose>
+								<xsl:choose>
+									<xsl:when test="name() = 'id'"><a href="#{../id}"><img src="http://dev.eclipse.org/viewcvs/indextools.cgi/%7Echeckout%7E/emf-home/images/icon-file-detail.gif" alt="More details below" width="14" height="16" border="0"/></a>&#160;<a href="#{../id}"><xsl:value-of select="." /></a></xsl:when>
+									<xsl:when test="name() = 'sev'"><xsl:value-of select="substring(.,1,3)" /></xsl:when>
+									<xsl:when test="name() = 'opened'"><xsl:value-of select="substring(.,1,10)" /></xsl:when>
+									<xsl:otherwise><xsl:value-of select="." /></xsl:otherwise>
+								</xsl:choose>
+								</td>
+							</xsl:if>
+						</xsl:for-each>
+					</tr>
+				</xsl:if>
+			</xsl:for-each>
+			<tr><td class="spacer"><br/></td><td class="spacer"><br/></td></tr>
+		</xsl:if>
+		<xsl:if test="count(key('bugEntry',@product)) = 0">
+			<tr><td colspan="3" class="normal">n/a</td></tr>
+		</xsl:if>
+	</xsl:for-each>
+	</table>
+
+	<p> </p>
+
+	<!-- content! -->
+	<table border="0" cellspacing="1" cellpadding="5" width="100%">
+	<xsl:for-each select="product-def">
+		<xsl:if test="((count(key('bugEntry',@product)) != 0 and $product = product) or $product = '')">
+			<tr class="header2">
+				<td colspan="12" class="sub-header">
+					<a name="{@product}"><xsl:value-of select="@label"/></a>: Details (<xsl:value-of select="count(key('bugEntry',@product))" /> Bugs) - <a class="sub-header" href="#{@product}_dev">see summary</a>
+				</td>
+			</tr>
+			<tr class="content-header2">
+				<xsl:for-each select="//column-def">
+					<xsl:if test="@column != 'product' and @column != 'summary' and @column != 'plan-comments'">
+						<td colspan="1" class="sub-header">
+							<xsl:value-of select="@label" />
+						</td>
+					</xsl:if>
+				</xsl:for-each>
+			</tr>
+			<xsl:for-each select="key('bugEntry',@product)">
+				<xsl:sort select="plan-priority" data-type="text" order="ascending" />
+				<xsl:if test="(starts-with(version,$version) or $version = '') and ($product = product or $product = '')">
+					<tr valign="top">
+						<xsl:choose>
+						<xsl:when test="(position() mod 2 = 1)">
+							<xsl:attribute name="class">dark-row2</xsl:attribute>
+						</xsl:when>
+						<xsl:otherwise>
+							<xsl:attribute name="class">light-row</xsl:attribute>
+						</xsl:otherwise>
+						</xsl:choose>
+						<xsl:for-each select="./*">
+							<xsl:if test="name() != 'product' and name() != 'summary' and name() != 'plan-comments'">
+								<td>
+								<xsl:choose>
+									<xsl:when test="name() = 'sev' and substring(../sev,1,3) = 'enh'">
+										<xsl:attribute name="class">italic</xsl:attribute>
+									</xsl:when>
+									<xsl:otherwise><xsl:attribute name="class">normal</xsl:attribute></xsl:otherwise>
+								</xsl:choose>
+								<xsl:choose>
+									<xsl:when test="name() = 'id'"><a name="{../id}"></a><a href="http://bugs.eclipse.org/bugs/show_bug.cgi?id={.}" target="_bugz"><xsl:value-of select="." /></a></xsl:when>
+									<xsl:when test="name() = 'sev'"><xsl:value-of select="substring(.,1,3)" /></xsl:when>
+									<xsl:when test="name() = 'opened'"><xsl:value-of select="substring(.,1,10)" /></xsl:when>
+									<xsl:otherwise><xsl:value-of select="." /></xsl:otherwise>
+								</xsl:choose>
+								</td>
+							</xsl:if>
+						</xsl:for-each>
+					</tr>
+					<tr>
+						<xsl:choose>
+						<xsl:when test="(position() mod 2 = 1)">
+							<xsl:attribute name="class">dark-row2</xsl:attribute>
+						</xsl:when>
+						<xsl:otherwise>
+							<xsl:attribute name="class">light-row</xsl:attribute>
+						</xsl:otherwise>
+						</xsl:choose>
+						<td colspan="1" 
+							class="normal">&#160;<a href="http://bugs.eclipse.org/bugs/show_bug.cgi?id={id}" target="_bugz"><img src="http://dev.eclipse.org/viewcvs/indextools.cgi/%7Echeckout%7E/emf-home/images/icon-file-edit.gif" alt="Edit Bug" width="16" height="16" border="0"/></a>&#160;</td>
+						<td colspan="9" class="normal">
+							<a href="http://bugs.eclipse.org/bugs/long_list.cgi?buglist={id}" target="_bugz" class="italic"><img src="http://dev.eclipse.org/viewcvs/indextools.cgi/%7Echeckout%7E/emf-home/images/icon-file.gif" alt="Full Bug Summary" width="13" height="16" border="0"/></a>&#160;<xsl:value-of select="summary" />
+						</td>
+						<td colspan="2" class="normal">
+							<xsl:value-of select="plan-comments" />
+						</td>
+					</tr>
+				</xsl:if>
+			</xsl:for-each>
+			<tr><td class="spacer"><br/></td><td class="spacer"><br/></td></tr>
+		</xsl:if>
+		<xsl:if test="count(key('bugEntry',@product)) = 0">
+			<tr><td colspan="3" class="normal">n/a</td></tr>
+		</xsl:if>
+	</xsl:for-each>
+	</table>
+
+<xsl:if test="$showFiltersOrHeaderFooter!='1'">
+
+<p>
+	<a name="quicknav">
+	<a href="../">EMF Home</a> |
+	<a href="../../xsd">XSD Home</a> | 
+	<a href="#top">Top of Page</a>
+	</a>
+</p>
+
+<!-- wrapper for left nav -->
+</xsl:if>
+
+</td></tr></table>
+
+</body>
+</html>
+</xsl:for-each>
+</xsl:template>
+
+</xsl:stylesheet>
+<!-- $Id: plan.xsl,v 1.1 2005/02/19 07:30:00 nickb Exp $ -->
