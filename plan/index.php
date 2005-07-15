5bfc39fee44c598411c4f4a7a4d122d0784e68ce
nickb <nickb>
2005-07-15 19:26:10 +0000

    *** empty log message ***

--- plan/index.php
+++ plan/index.php
@@ -22,7 +22,7 @@
 			echo "<tr><td colspan=\"3\"><b>Choose plan version:</b></td></tr>";
 			rsort($vers);
 			foreach ($vers as $ver) { 
-				echo '<tr><td> &#149; <a href="/emf/plan/'.$ver.'">EMF Development Plan '.preg_replace("/plan-([\d\.]+)\.xml/","",$ver).'</td></tr>';
+				echo '<tr><td> &#149; <a href="/emf/plan/'.$ver.'">EMF Development Plan '.preg_replace("/plan-([\d\.]+)\.xml/","$1",$ver).'</td></tr>';
 			}
 			echo "</table>\n";
 		} else {
