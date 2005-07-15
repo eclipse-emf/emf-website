d522f8964fdc8feb5a35b8f0780cf99b9390da54
nickb <nickb>
2005-07-15 18:57:57 +0000

    new index

---
+++ plan/index.php
@@ -0,0 +1,56 @@
+<?php 
+	/* if on www.eclipse.org, redirect to download; if on download or mirror, present a list of avail javadoc versions available */
+	/* if querystring value, pick latest version of javadoc and serve up that page */
+
+	$isWWWserver = $WWWserver!="false"&&($SERVER_NAME=="www.eclipse.org"||$SERVER_NAME=="eclipse.org");
+
+	$folder = substr($SCRIPT_NAME,0,strrpos($SCRIPT_NAME,"/"));
+	
+	if (!$isWWWserver) { 
+		header("Location: http://eclipse.org/emf/plan/");
+		exit;
+	} else {
+		$vers = loadDirSimple("./","(\d\.\d|\d\.\d\.\d+)","f");
+		if (sizeof($vers)>0) { ?><!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Eclipse Tools - EMF Development Plans</title>
+<link rel="stylesheet" href="http://www.eclipse.org/default_style.css" type="text/css">
+</head>
+<body>
+<?php
+			echo "<table>\n";
+			echo "<tr><td colspan=\"3\"><b>Choose plan version:</b></td></tr>";
+			rsort($vers);
+			foreach ($vers as $ver) { 
+				echo '<tr><td> &#149; <a href="/emf/plan/'.$ver.'">EMF Development Plan '.preg_replace("/plan-([\d\.]+)\.xml/","",$ver).'</td></tr>';
+			}
+			echo "</table>\n";
+		} else {
+			echo "No plan docs found!";
+		}
+	}
+
+/**********************/
+
+function loadDirSimple($dir,$ext,$type) { // 1D array
+	$stuff = array();
+	if (is_dir($dir) && is_readable($dir)) { 
+		ini_set("display_errors","0"); // suppress file not found errors
+		$handle=opendir($dir);
+		while (($file = readdir($handle))!==false) {
+		  if ( ($ext=="" || preg_match("/".$ext."$/",$file)) && $file!=".." && $file!="." && $type=="f") { 
+			  $stuff[] = "$file"; 
+		  } else if ( ($ext=="" || preg_match("/".$ext."$/",$file)) && $file!=".." && $file!="." && $type=="d") {
+			  $stuff[] = "$file"; 
+		  }
+		}
+		closedir($handle); 
+		ini_set("display_errors","1"); // and turn 'em back on.
+	} else {
+		//exit;
+	}
+	return $stuff;
+}
+?>
