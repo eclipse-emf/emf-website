c8de651f5511c2d0ea82e9416a84f463cbc68ffc
nickb <nickb>
2005-07-19 22:13:07 +0000

    *** empty log message ***

--- plan/devplan.php
+++ plan/devplan.php
@@ -21,38 +21,58 @@
 
 	# Please note: some columns are not SELECTable, such as the password and e-mail address.
 	# They will return an error.
-	$sql_info = "SELECT DISTINCT
-						BUG.bug_id, 
-						BUG.short_desc,
-						USR.realname AS somedude
-				FROM 
-						bugs AS BUG
-						INNER JOIN profiles AS USR ON USR.userid = BUG.reporter
-				WHERE
-						BUG.bug_id = $bug";
-	
+	$query = "SELECT DISTINCT
+	BUG.bug_id,
+	PROD.name as PNAME,
+	CMP.name as CNAME,
+	BUG.short_desc,
+	BUG.bug_severity,
+	BUG.bug_status,
+	BUG.resolution,
+	BUG.creation_ts,
+	BUG.delta_ts,
+	BUG.lastdiffed,
+	BUG.estimated_time,
+	BUG.remaining_time,
+	BUG.priority,
+	BUG.version,
+	BUG.target_milestone,
+	BUG.votes,
+	PROF.realname
+FROM 
+	bugs as BUG,
+	profiles as PROF,
+	bugs_activity as ACT,
+	products as PROD,
+	components as CMP,
+	longdescs as TXT
+WHERE
+	BUG.reporter = PROF.userid AND
+	CMP.id = BUG.component_id AND
+	PROD.id = BUG.product_id AND
+	BUG.bug_id = TXT.bug_id AND
+	BUG.bug_id = ACT.bug_id AND
+	BUG.bug_id = $bug";
+
 	$rs 	= mysql_query($sql_info, $dbh);
 	
 	if(mysql_errno($dbh) > 0) {
 		echo "There was an error processing the request:\n\n$query".
-		
-		# For debugging purposes - don't display this stuff in a production page.
-		# echo "Error: ".mysql_error($dbh)."\n";
-		
 		# Mysql disconnects automatically, but I like my disconnects to be explicit.
 		$dbc->disconnect();
+		$dbh = null;
+		$dbc = null;
 		exit;
 	}
-		
+
 	while($myrow = mysql_fetch_assoc($rs)) {
 		foreach ($myrow as $k => $v) { 
 			echo "$k => $v\n";
 		}
 	}
-	
-	$dbc->disconnect();
 
-	$rs 		= null;
-	$dbh 		= null;
-	$dbc 		= null;
+	$dbc->disconnect();
+	$rs  = null;
+	$dbh = null;
+	$dbc = null;
 ?>
