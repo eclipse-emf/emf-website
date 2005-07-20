d3ebce4203a759091358911e12f4a2d25462a0c1
nickb <nickb>
2005-07-20 01:23:12 +0000

    add canned queries

--- plan/query.php
+++ plan/query.php
@@ -14,11 +14,21 @@
 
 	$bug = $_GET["bug"]; if (!$bug) { $bug="61639"; }
 	$query = $_POST["query"];
-	
-	$query = ($query?$query:"SELECT DISTINCT
+	echo '
+<html>
+<head></head>
+<body>
+<table><form method=post><tr valign="top"><td align="left">
+	<pre>Query:</pre>
+	<textarea style="font-size:12px" name=query rows=40 cols=60>'.$query.'</textarea><pre style="font-size:12px;color:navy">
+
+#--------#--------#--------#--------
+# get bug details for a given bug
+
+SELECT DISTINCT
   BUG.bug_id, 
   PROD.name as PNAME, CMP.name as CNAME, 
-  PROF.realname, 
+  PROF.userid, PROF.realname, 
   BUG.short_desc, 
   BUG.priority, BUG.bug_severity, 
   BUG.bug_status, BUG.resolution, 
@@ -27,30 +37,76 @@
 FROM 
   bugs as BUG, 
   profiles as PROF, 
-  bugs_activity as ACT, 
   products as PROD, 
-  components as CMP, 
-  longdescs as TXT 
+  components as CMP
 WHERE 
   BUG.reporter = PROF.userid AND 
   CMP.id = BUG.component_id AND 
   PROD.id = BUG.product_id AND 
-  BUG.bug_id = TXT.bug_id AND 
-  BUG.bug_id = ACT.bug_id AND 
-  BUG.bug_id = $bug");
+  BUG.bug_id = 61639
 
-	echo '
-<html>
-<head></head>
-<body>
-<table><form method=post><tr valign="top"><td align="left">
-	<pre>Query:</pre>
-	<textarea style="font-size:12px" name=query rows=40 cols=60>'.$query.'</textarea><br>
+#--------#--------#--------#--------
+# get activity for a given bug
+
+SELECT DISTINCT 
+  ACT.who, ACT.bug_when, ACT.fieldid, ACT.added, ACT.removed
+FROM 
+  bugs as BUG,
+  bugs_activity as ACT
+WHERE 
+  BUG.bug_id = ACT.bug_id AND 
+  BUG.bug_id = 61639
+ORDER BY
+  bug_when
+ASC
+
+#--------#--------#--------#--------
+# get bug comments (Description and Additional Comments) for a given bug
+
+SELECT DISTINCT
+  PROF.realname, TXT.bug_when, TXT.thetext
+FROM 
+  bugs as BUG, 
+  profiles as PROF, 
+  longdescs as TXT 
+WHERE 
+  TXT.who = PROF.userid AND 
+  BUG.bug_id = TXT.bug_id AND 
+  BUG.bug_id = 61639
+ORDER BY
+  bug_when
+ASC
+
+#--------#--------#--------#--------
+# get committer name/details for a given id, from comments
+
+SELECT DISTINCT
+  PROF.userid, PROF.realname
+FROM 
+  profiles as PROF, 
+  longdescs as TXT 
+WHERE 
+  TXT.who = PROF.userid AND 
+  TXT.who = 2253
+
+#--------#--------#--------#--------
+# get committer name/details for a given id, from bug
+
+SELECT DISTINCT
+  PROF.userid, PROF.realname
+FROM 
+  profiles as PROF, 
+  bugs as BUG
+WHERE 
+  BUG.reporter = PROF.userid AND 
+  BUG.reporter = 2253
+
+</pre>
 	<input type=submit name="Submit" style="font-size:12px">
 </td><td>&nbsp;&nbsp;</td>
-<td><pre>Results:</pre>';
-	
+<td>';
 	if ($query) { 
+		echo "<pre>Results:</pre>\n";
 		echo "<pre style=\"color:blue\">";
 		# Connect to database
 		$dbc 	= new DBConnectionBugs();
@@ -59,7 +115,7 @@ WHERE
 		$rs 	= mysql_query($query, $dbh);
 		
 		if(mysql_errno($dbh) > 0) {
-			echo "There was an error processing the request:\n\n$query\n\n".
+			echo "There was an error processing the query.</pre>\n".
 			# Mysql disconnects automatically, but I like my disconnects to be explicit.
 			$dbc->disconnect();
 			$dbh = null;
@@ -68,11 +124,13 @@ WHERE
 		}
 			
 		while($myrow = mysql_fetch_assoc($rs)) {
+			echo "<hr noshade size=1/>\n";
 			foreach ($myrow as $k => $v) { 
 				echo "$k => $v\n";
 			}
 		}
 		
+		echo "</pre>";
 		$dbc->disconnect();
 		$rs  = null;
 		$dbh = null;
@@ -80,7 +138,6 @@ WHERE
 	}
 
 	echo '
-</pre>
 </td></tr></form></table>
 </body>
 </html>
