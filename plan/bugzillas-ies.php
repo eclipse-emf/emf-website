897bb7c338282bb4a2d25029103621c8fb2c6c85
nickb <nickb>
2005-07-27 03:36:35 +0000

    sql formatting

--- plan/bugzillas-ies.php
+++ plan/bugzillas-ies.php
@@ -27,8 +27,7 @@ FROM
 WHERE 
   PROD.id = BUG.product_id AND 
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
-  BUG.creation_ts >= '2004-07-01' 
-    AND BUG.creation_ts <= '2005-07-07'",
+  BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07'",
 
 "Defects Fixed or Resolved of Those Entered, 2004-07-01 to 2005-07-07 (GA)" =>
 "SELECT DISTINCT
@@ -39,11 +38,9 @@ FROM
 WHERE 
   PROD.id = BUG.product_id AND 
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
-  BUG.creation_ts >= '2004-07-01' 
-    AND BUG.creation_ts <= '2005-07-07'
+  BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
 
-    AND (BUG.bug_status = 'RESOLVED' OR 
-      BUG.resolution = 'FIXED')",
+    (BUG.bug_status = 'RESOLVED' OR BUG.resolution = 'FIXED')",
 
 "Defects NOT Fixed or Resolved of Those Entered, 2004-07-01 to 2005-07-07 (GA)" =>
 "SELECT DISTINCT
@@ -54,11 +51,9 @@ FROM
 WHERE 
   PROD.id = BUG.product_id AND 
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
-  BUG.creation_ts >= '2004-07-01' 
-    AND BUG.creation_ts <= '2005-07-07'
+  BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
 
-    AND (BUG.bug_status != 'RESOLVED' AND 
-      BUG.resolution != 'FIXED')",
+    (BUG.bug_status != 'RESOLVED' AND BUG.resolution != 'FIXED')",
 
 "Defects Fixed or Resolved Between 2004-07-01 and 2005-07-07 (GA) (including older bugs)" =>
 "SELECT DISTINCT 
@@ -72,13 +67,11 @@ WHERE
   FLD.fieldid = ACT.fieldid AND
   PROD.id = BUG.product_id AND 
   BUG.bug_id = ACT.bug_id AND 
-  ACT.bug_when >= '2004-07-01' AND
-  ACT.bug_when <= '2005-07-07' AND
+  ACT.bug_when >= '2004-07-01' AND ACT.bug_when <= '2005-07-07' AND
+
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
-    ( (FLD.description = 'Resolution' AND 
-       ACT.added = 'FIXED') OR
-      (FLD.description = 'Status' AND 
-       ACT.added = 'RESOLVED') 
+    ( (FLD.description = 'Resolution' AND ACT.added = 'FIXED') OR
+      (FLD.description = 'Status' AND ACT.added = 'RESOLVED') 
     )",
 
 "Critical Defects NOT Fixed or Resolved of Those Entered, 2004-07-01 to 2005-07-07 (GA)" =>
@@ -90,11 +83,10 @@ FROM
 WHERE 
   PROD.id = BUG.product_id AND 
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
-  BUG.creation_ts >= '2004-07-01' 
-    AND BUG.creation_ts <= '2005-07-07'
-    AND (BUG.bug_status != 'RESOLVED' AND
-      BUG.resolution != 'FIXED')
-    AND BUG.bug_severity = 'critical'",
+  BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
+    (BUG.bug_status != 'RESOLVED' AND BUG.resolution != 'FIXED') AND
+
+    BUG.bug_severity = 'critical'",
 
 "Blocker Defects NOT Fixed or Resolved of Those Entered, 2004-07-01 to 2005-07-07 (GA)" =>
 "SELECT DISTINCT
@@ -105,11 +97,10 @@ FROM
 WHERE 
   PROD.id = BUG.product_id AND 
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
-  BUG.creation_ts >= '2004-07-01' 
-    AND BUG.creation_ts <= '2005-07-07'
-    AND (BUG.bug_status != 'RESOLVED' AND
-      BUG.resolution != 'FIXED')
-    AND BUG.bug_severity = 'blocker'",
+  BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
+    (BUG.bug_status != 'RESOLVED' AND BUG.resolution != 'FIXED') AND
+
+    BUG.bug_severity = 'blocker'",
 
 "P1 Defects NOT Fixed or Resolved of Those Entered, 2004-07-01 to 2005-07-07 (GA)" =>
 "SELECT DISTINCT
@@ -120,11 +111,10 @@ FROM
 WHERE 
   PROD.id = BUG.product_id AND 
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
-  BUG.creation_ts >= '2004-07-01' 
-    AND BUG.creation_ts <= '2005-07-07'
-    AND (BUG.bug_status != 'RESOLVED' AND
-      BUG.resolution != 'FIXED')
-    AND BUG.priority = 'P1'"
+  BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
+    (BUG.bug_status != 'RESOLVED' AND BUG.resolution != 'FIXED') AND
+
+    BUG.priority = 'P1'"
 
 );
 
@@ -135,14 +125,14 @@ echo '
 foreach ($queries as $label => $query) {
 
 	echo '
-<tr><td colspan="1"><pre style="font-size:12px;color:#009900">'.$label.'</pre></td></tr>
+<tr><td colspan="3"><pre style="font-size:12px;color:blue">'.$label.'</pre></td></tr>
 	
 <tr valign="top">
 	<td><pre style="font-size:12px;color:navy">'.$query.'</pre></td>
 	<td>&nbsp;&nbsp;</td>
 	<td>';
 	if ($query) { 
-		echo "<pre style=\"color:blue\">";
+		echo "<pre style=\"color:#008000\">";
 		# Connect to database
 		$dbc 	= new DBConnectionBugs();
 		$dbh 	= $dbc->connect();
