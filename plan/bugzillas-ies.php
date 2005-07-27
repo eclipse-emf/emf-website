3d895d8269381c62230c5a8a488a80cdcc654924
nickb <nickb>
2005-07-27 18:13:48 +0000

    reverse

--- plan/bugzillas-ies.php
+++ plan/bugzillas-ies.php
@@ -1,17 +1,17 @@
 <?php
-	# Props to Denis Roy (webmaster@eclipse.org) for the basecode from which this script grew.
-	# Logic, DB and Presentation lumped here for simplicity.
-	# Please avoid using aggregate functions (COUNT, SUM, MAX, MIN, etc) on busy web pages.
-	# For bugzilla this is not critical as the tables are small (<10,000,000 records)
-	# but imagine if every project displays a COUNT(*) for their project's bugs right on the front page!
-	# ** NOTE ** You need to tell the WebMaster from which URL you are loading this class from, 
-	# otherwise the connect() will fail.
-	
-	# Load up the classfile
-	require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
+   # Props to Denis Roy (webmaster@eclipse.org) for the basecode from which this script grew.
+   # Logic, DB and Presentation lumped here for simplicity.
+   # Please avoid using aggregate functions (COUNT, SUM, MAX, MIN, etc) on busy web pages.
+   # For bugzilla this is not critical as the tables are small (<10,000,000 records)
+   # but imagine if every project displays a COUNT(*) for their project's bugs right on the front page!
+   # ** NOTE ** You need to tell the WebMaster from which URL you are loading this class from, 
+   # otherwise the connect() will fail.
+   
+   # Load up the classfile
+   require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
 
-	header("Content-Type: text/html");
-	echo '
+   header("Content-Type: text/html");
+   echo '
 <html>
 <head></head>
 <body>
@@ -40,11 +40,11 @@ WHERE
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
   BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
     (
-		BUG.bug_status = 'RESOLVED' OR BUG.bug_status = 'VERIFIED' OR
-		BUG.bug_status = 'CLOSED' OR 
-		BUG.resolution = 'FIXED' OR BUG.resolution = 'INVALID' OR
-		BUG.resolution = 'WONTFIX' OR BUG.resolution = 'WORKSFORME'
-	 )",
+      BUG.bug_status = 'RESOLVED' OR BUG.bug_status = 'VERIFIED' OR
+      BUG.bug_status = 'CLOSED' OR 
+      BUG.resolution = 'FIXED' OR BUG.resolution = 'INVALID' OR
+      BUG.resolution = 'WONTFIX' OR BUG.resolution = 'WORKSFORME'
+    )",
 
 "Defects NOT Fixed/Resolved/Closed of Those Entered, 2004-07-01 to 2005-07-07 (GA)" =>
 "SELECT DISTINCT
@@ -57,11 +57,11 @@ WHERE
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
   BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
     (
-		BUG.bug_status = 'RESOLVED' OR BUG.bug_status = 'VERIFIED' OR
-		BUG.bug_status = 'CLOSED' OR 
-		BUG.resolution = 'FIXED' OR BUG.resolution = 'INVALID' OR
-		BUG.resolution = 'WONTFIX' OR BUG.resolution = 'WORKSFORME'
-	 )",
+      BUG.bug_status != 'RESOLVED' AND BUG.bug_status != 'VERIFIED' AND
+      BUG.bug_status != 'CLOSED' AND 
+      BUG.resolution != 'FIXED' AND BUG.resolution != 'INVALID' AND
+      BUG.resolution != 'WONTFIX' AND BUG.resolution != 'WANDKSFANDME'
+    )",
 
 "Defects Fixed/Resolved/Closed Between 2004-07-01 and 2005-07-07 (GA) (including older bugs)" =>
 "SELECT DISTINCT 
@@ -97,11 +97,11 @@ WHERE
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
   BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
     (
-		BUG.bug_status != 'RESOLVED' AND BUG.bug_status != 'VERIFIED' AND
-		BUG.bug_status != 'CLOSED' AND 
-		BUG.resolution != 'FIXED' AND BUG.resolution != 'INVALID' AND
-		BUG.resolution != 'WONTFIX' AND BUG.resolution != 'WANDKSFANDME'
-	 ) AND
+      BUG.bug_status != 'RESOLVED' AND BUG.bug_status != 'VERIFIED' AND
+      BUG.bug_status != 'CLOSED' AND 
+      BUG.resolution != 'FIXED' AND BUG.resolution != 'INVALID' AND
+      BUG.resolution != 'WONTFIX' AND BUG.resolution != 'WANDKSFANDME'
+    ) AND
     BUG.bug_severity = 'critical'",
 
 "Blocker Defects NOT Fixed/Resolved/Closed of Those Entered, 2004-07-01 to 2005-07-07 (GA)" =>
@@ -115,11 +115,11 @@ WHERE
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
   BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
     (
-		BUG.bug_status != 'RESOLVED' AND BUG.bug_status != 'VERIFIED' AND
-		BUG.bug_status != 'CLOSED' AND 
-		BUG.resolution != 'FIXED' AND BUG.resolution != 'INVALID' AND
-		BUG.resolution != 'WONTFIX' AND BUG.resolution != 'WANDKSFANDME'
-	 ) AND
+      BUG.bug_status != 'RESOLVED' AND BUG.bug_status != 'VERIFIED' AND
+      BUG.bug_status != 'CLOSED' AND 
+      BUG.resolution != 'FIXED' AND BUG.resolution != 'INVALID' AND
+      BUG.resolution != 'WONTFIX' AND BUG.resolution != 'WANDKSFANDME'
+    ) AND
     BUG.bug_severity = 'blocker'",
 
 "P1 Defects NOT Fixed/Resolved/Closed of Those Entered, 2004-07-01 to 2005-07-07 (GA)" =>
@@ -133,11 +133,11 @@ WHERE
   (PROD.name = 'EMF' OR PROD.name = 'XSD') AND
   BUG.creation_ts >= '2004-07-01' AND BUG.creation_ts <= '2005-07-07' AND
     (
-		BUG.bug_status != 'RESOLVED' AND BUG.bug_status != 'VERIFIED' AND
-		BUG.bug_status != 'CLOSED' AND 
-		BUG.resolution != 'FIXED' AND BUG.resolution != 'INVALID' AND
-		BUG.resolution != 'WONTFIX' AND BUG.resolution != 'WANDKSFANDME'
-	 ) AND
+      BUG.bug_status != 'RESOLVED' AND BUG.bug_status != 'VERIFIED' AND
+      BUG.bug_status != 'CLOSED' AND 
+      BUG.resolution != 'FIXED' AND BUG.resolution != 'INVALID' AND
+      BUG.resolution != 'WONTFIX' AND BUG.resolution != 'WANDKSFANDME'
+    ) AND
     BUG.priority = 'P1'"
 
 );
@@ -148,48 +148,48 @@ echo '
 
 $cnt=0;
 foreach ($queries as $label => $query) {
-	$cnt++;
+   $cnt++;
 
-	echo '
+   echo '
 <tr><td colspan="3">
-	<pre style="font-size:12px;color:red">'.$cnt.'. </pre><pre style="font-size:12px;color:blue">'.$label.'</pre></td></tr>
-	
+   <pre style="font-size:12px;color:red">'.$cnt.'. </pre><pre style="font-size:12px;color:blue">'.$label.'</pre></td></tr>
+   
 <tr valign="top">
-	<td><pre style="font-size:12px;color:navy">'.$query.'</pre></td>
-	<td>&nbsp;&nbsp;</td>
-	<td>';
-	if ($query) { 
-		echo "<pre style=\"color:#008000\">";
-		# Connect to database
-		$dbc 	= new DBConnectionBugs();
-		$dbh 	= $dbc->connect();
+   <td><pre style="font-size:12px;color:navy">'.$query.'</pre></td>
+   <td>&nbsp;&nbsp;</td>
+   <td>';
+   if ($query) { 
+      echo "<pre style=\"color:#008000\">";
+      # Connect to database
+      $dbc  = new DBConnectionBugs();
+      $dbh  = $dbc->connect();
 
-		$rs 	= mysql_query($query, $dbh);
-		
-		if(mysql_errno($dbh) > 0) {
-			echo "There was an error processing the query.</pre>\n".
-			# Mysql disconnects automatically, but I like my disconnects to be explicit.
-			$dbc->disconnect();
-			$dbh = null;
-			$dbc = null;
-			exit;
-		}
-			
-		while($myrow = mysql_fetch_assoc($rs)) {
-			foreach ($myrow as $k => $v) { 
-				echo "$k => $v\n";
-			}
-		}
-		
-		echo "</pre>";
-		$dbc->disconnect();
-		$rs  = null;
-		$dbh = null;
-		$dbc = null;
-	}
+      $rs   = mysql_query($query, $dbh);
+      
+      if(mysql_errno($dbh) > 0) {
+         echo "There was an error processing the query.</pre>\n".
+         # Mysql disconnects automatically, but I like my disconnects to be explicit.
+         $dbc->disconnect();
+         $dbh = null;
+         $dbc = null;
+         exit;
+      }
+         
+      while($myrow = mysql_fetch_assoc($rs)) {
+         foreach ($myrow as $k => $v) { 
+            echo "$k => $v\n";
+         }
+      }
+      
+      echo "</pre>";
+      $dbc->disconnect();
+      $rs  = null;
+      $dbh = null;
+      $dbc = null;
+   }
 
-	echo '
-	</td>
+   echo '
+   </td>
 </tr>
 <tr><td colspan="3"><hr noshade="noshade" size="1"/></td></tr>
 ';
