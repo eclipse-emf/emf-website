84c859acb2d9208906b11417896e589dad45ddfc
nickb <nickb>
2005-11-21 19:48:20 +0000

    now add support for querystring vars

--- faq/faq2.php
+++ faq/faq2.php
@@ -20,9 +20,41 @@
 
 $vars = explode("&", $_SERVER['QUERY_STRING']);
 for ($i=0;$i<=count($vars);$i++) {
- $var = explode("=", $vars[$i]);
- $params[$var[0]] = $var[1];
+  $var = explode("=", $vars[$i]);
+  $qs[$var[0]] = $var[1];
 }
+
+$params = array();
+$params["FAQ"] = $qs["FAQ"];
+
+// simplified QS input for canned queries
+switch ($qs["FAQ"]) {
+	case "EMF":
+		$qs["Category"] = "emf";
+		$qs["Question"] = "EMF";
+		$qs["Answer"] = "EMF";
+		break;
+	case "SDO":
+		$qs["Category"] = "sdo";
+		$qs["Question"] = "SDO";
+		$qs["Answer"] = "SDO";
+		break;
+	case "XSD":
+		$qs["Category"] = "xsd";
+		$qs["Question"] = "XSD";
+		$qs["Answer"] = "XSD";
+		break;
+	default:
+		break;
+}
+
+// other mappings of filterNameX to filterNameValX
+$filterNames = array("","Category","Question","Answer");
+for ($i=1;$i<=3;$i++) { 
+  $fn = $filterNames[$i];
+  $params["filterName".$i] = $fn; // filterName1 = Category
+  $params["filterVal".$i] = $qs[$fn]; // filterVal1 = $qs["Category"]
+} 
 
 $processor = xslt_create();
 $fileBase = 'file://' . getcwd () . '/';
