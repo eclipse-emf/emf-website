6151eb1e9e4fc32dabc3631da9e786219c225562
nickb <nickb>
2006-01-20 20:16:46 +0000

    add header/footer

--- faq/faq2.php
+++ faq/faq2.php
@@ -1,5 +1,41 @@
-<?php
+<?php 
 
+if ($FAQ=="XSD") { 
+	$HTMLTitle = "XML Schema Infoset Model FAQ";
+	$ProjectName = array(
+		"XML Schema Infoset Model FAQ",
+		"XML Schema Infoset Model FAQ",
+		"XML Schema Infoset Model FAQ",
+		"images/reference.gif"
+		);
+} else if ($FAQ=="SDO") { 
+	$HTMLTitle = "Service Data Objects FAQ";
+	$ProjectName = array(
+		"Service Data Objects FAQ",
+		"Service Data Objects FAQ",
+		"Service Data Objects FAQ",
+		"images/reference.gif"
+		);
+} else if ($FAQ=="EMF") { 
+	$HTMLTitle = "Eclipse Modeling Framework FAQ";
+	$ProjectName = array(
+		"Eclipse Modeling Framework FAQ",
+		"Eclipse Modeling Framework FAQ",
+		"Eclipse Modeling Framework FAQ",
+		"images/reference.gif"
+		);
+} else {
+	$HTMLTitle = "Eclipse Modeling Framework FAQ";
+	$ProjectName = array(
+		"Eclipse Modeling Framework FAQ",
+		"Eclipse Modeling Framework FAQ",
+		"Eclipse Modeling Framework FAQ",
+		"images/reference.gif"
+		);
+}
+$pre = "../";
+include $pre . "includes/header.php"; 
+		
 /*
  * To work, this script must be run with a version of PHP4 which
  * includes the Sablotron XSLT extension compiled into it
@@ -26,6 +62,7 @@ for ($i=0;$i<=count($vars);$i++) {
 
 $params = array();
 $params["FAQ"] = $qs["FAQ"];
+$params["showFiltersOrHeaderFooter"] = 1;
 
 // simplified QS input for canned queries
 switch ($qs["FAQ"]) {
@@ -59,9 +96,13 @@ for ($i=1;$i<=3;$i++) {
 $processor = xslt_create();
 $fileBase = 'file://' . getcwd () . '/';
 xslt_set_base ( $processor, $fileBase );
-$result = xslt_process($processor, $fileBase.'faq.xml', $fileBase.'faq.xsl', NULL, array(), $params);
+$XMLfile = "faq.xml";
+$result = xslt_process($processor, $fileBase.$XMLfile, $fileBase.'faq.xsl', NULL, array(), $params);
 
 if(!$result) echo xslt_errno($processor) . " : " . xslt_error($processor);
-echo $result;
+echo $result; ?>
 
-?>
+<p><a href="view-source:http://eclipse.org/emf/faq/<?php echo $XMLfile; ?>" class="red">View as XML</a></p>
+
+<?php include $pre . "includes/footer.php"; ?>
+<!-- $Id: faq2.php,v 1.4 2006/01/20 20:16:46 nickb Exp $ -->
