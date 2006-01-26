01500b096244990fd0231f75a89df0c35e109c53
nickb <nickb>
2006-01-26 21:42:41 +0000

    first query

--- downloads/stats.php
+++ downloads/stats.php
@@ -1,42 +1,62 @@
  <?php
-        # Sample PHP code to issue a Downloads query.
-        # Logic, DB and Presentation lumped here for simplicity.
-        # Load up the classfile
-        # You need to tell the WebMaster from which URL you are loading this class from, 
-        # otherwise the connect() will fail.
-        require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_downloads_ro.class.php";
-        
-        # Connect to database
-        $dbc    = new DBConnectionDownloads();
-        $dbh    = $dbc->connect();
 
-        # There are usually in excess of 30 million records.. watch your queries!!
-        $sql_info = "SELECT 
-						DOW.file, 
-                        COUNT(*) AS RecordCount
-                     FROM 
-                     	downloads AS DOW
-			         WHERE
-                        DOW.file LIKE \"%emf-sdo-xsd-SDK-2.2.0M%\"";
-        
-        $rs     = mysql_query($sql_info, $dbh);
-        
-        if(mysql_errno($dbh) > 0) {
-			echo "There was an error processing this request";
-			# For debugging purposes - don't display this stuff in a production page.
-			# echo mysql_error($dbh);
-			# Mysql disconnects automatically, but I like my disconnects to be explicit.
-			$dbc->disconnect();
-			exit;
-        }
-                
-        while($myrow = mysql_fetch_assoc($rs)) {
-           echo "File: " . $myrow['file'] . " Count: " . $myrow['RecordCount'] . "<br>\n";
-        }
-        
-        $dbc->disconnect();
+require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_downloads_ro.class.php";
 
-        $rs             = null;
-        $dbh            = null;
-        $dbc            = null;
+$queries = array(
+	"Zips" => "SELECT DOW.file as Zipfile, COUNT(*) AS Requests FROM downloads AS DOW WHERE
+                DOW.file LIKE \"%emf-sdo-xsd-SDK-2.2.0M%\" GROUP BY DOW.file", 
+	"Countries" => "SELECT DOW.remote_host as Requester, COUNT(*) AS Requests FROM downloads AS DOW WHERE
+                DOW.file LIKE \"%emf-sdo-xsd-SDK-2.2.0M%\" GROUP BY DOW.file"
+);
+
+foreach ($queries as $title => $query) { 
+	$results = doQuery($query);
+	echo "<hr noshade=\"noshade\" size=\"1\"/>";
+	echo "<p><table cellspacing=\"0\" cellpadding=\"2\"><tr><td colspan=\"\">$title</td></tr>";
+	foreach ($results as $i => $data) {
+   		if (!$i) { # do column header
+   			echo "<tr bgcolor=\"navy\">\n";
+			foreach ($data as $label => $datum) { 
+    			echo "\t<td><b style=\"color:white\">$label</b></td>\n";
+    		}
+   			echo "</tr>\n";
+		}
+		echo "<tr bgcolor=\"".($i%2==1?"#EEEEEE":"#FFFFFF")."\">\n";
+		foreach ($data as $label => $datum) { 
+			echo "\t<td>$datum</td>\n";
+		}
+		echo "</tr>\n";
+	}
+	echo "</table></p>";
+}
+        
+##########################################################################################
+        
+# There are usually in excess of 30 million records.. watch your queries!!
+function doQuery($sql) {
+	$arr = array();
+	
+    # Connect to database & get results set
+    $dbc = new DBConnectionDownloads(); $dbh = $dbc->connect(); $rs = mysql_query($sql, $dbh);
+    
+    if(mysql_errno($dbh) > 0) {
+		echo "There was an error processing this request";
+		# For debugging purposes - don't display this stuff in a production page.
+		# echo mysql_error($dbh);
+		# Mysql disconnects automatically, but I like my disconnects to be explicit.
+		$dbc->disconnect();
+		exit;
+    }
+            
+    while($myrow = mysql_fetch_assoc($rs)) { $arr[] = $myrow;
+       # echo "File: " . $myrow['file'] . " Count: " . $myrow['RecordCount'] . "<br>\n";
+    }
+    
+    # dsconnect and destroy objects
+    $dbc->disconnect();
+    $rs = null; $dbh = null; $dbc = null;
+    
+    return $arr;
+}
+
 ?>
