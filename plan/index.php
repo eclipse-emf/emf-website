36ce6bb6a842d2646d3ccfea44c5e124e9809d5f
nickb <nickb>
2006-05-19 15:27:01 +0000

    *** empty log message ***

--- plan/index.php
+++ plan/index.php
@@ -4,7 +4,7 @@
 
 	$isWWWserver = ($SERVER_NAME=="www.eclipse.org"||$SERVER_NAME=="eclipse.org");
 	
-	if (!$isWWWserver) { 
+	if (false || !$isWWWserver) { 
 		header("Location: http://www.eclipse.org/emf/plan/index.php");
 		exit;
 	} else {
