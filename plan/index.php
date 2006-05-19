11d5bde74796929d3a5de0835f09a96501360c49
nickb <nickb>
2006-05-19 15:25:40 +0000

    *** empty log message ***

--- plan/index.php
+++ plan/index.php
@@ -5,7 +5,7 @@
 	$isWWWserver = ($SERVER_NAME=="www.eclipse.org"||$SERVER_NAME=="eclipse.org");
 	
 	if (!$isWWWserver) { 
-		header("Location: http://www.eclipse.org/emf/plan/");
+		header("Location: http://www.eclipse.org/emf/plan/index.php");
 		exit;
 	} else {
 		$vers = loadDirSimple(".","plan-(\d\.\d|\d\.\d\.\d+)\.xml","f");
