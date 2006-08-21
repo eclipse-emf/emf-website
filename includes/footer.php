edc2d15d47487c861dcfe75172b84e93744bf3c4
nickb <nickb>
2006-08-21 21:27:04 +0000

    [153309] no logner used

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,3 @@
-<?php include $pre."includes/nav.php"; ?>
-
-<p>
-	<a href="<?php if ($isWWWserver) { ?>/emf<?php } else { ?>/tools/emf/scripts<?php } ?>/emf.php">EMF Home</a> |
-	<a href="<?php if ($isWWWserver) { ?>/emf<?php } else { ?>/tools/emf/scripts<?php } ?>/sdo.php">SDO Home</a> | 
-	<a href="<?php if ($isWWWserver) { ?>/emf<?php } else { ?>/tools/emf/scripts<?php } ?>/xsd.php">XSD Home</a> | 
-	<a href="#top">Top of Page</a>
-</p>
-
-<!-- wrapper for left nav -->
-</td></tr></table>
-
-</body></html>
+<?php 
+//no longer used
+?>
--- includes/nav.js
+++ includes/nav.js
@@ -1,4 +1,1 @@
-function goBugzLink() {
-	var collist = "%26query_format%3Dadvanced&column_changeddate=on&column_bug_severity=on&column_priority=on&column_rep_platform=on&column_bug_status=on&column_product=on&column_component=on&column_version=on&column_target_milestone=on&column_short_short_desc=on&splitheader=0";
-	document.location.href='https://bugs.eclipse.org/bugs/colchange.cgi?rememberedquery=product%3DEMF%2CXSD%26bug_status%3DNEW%26bug_status%3DASSIGNED%26bug_status%3DREOPENED%26order%3Dbugs.bug_status%2Cbugs.target_milestone%2Cbugs.bug_id'+collist;
-}
+//no longer used
