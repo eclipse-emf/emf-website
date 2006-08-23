3682a3ffe6096d4cecb6775910daf637f424c048
nickb <nickb>
2006-08-23 01:49:55 +0000

    *** empty log message ***

--- models/models-mailform.php
+++ models/models-mailform.php
@@ -1,26 +1,28 @@
 <?php 
 function wmail($toname, $to, $fromname, $from, $subject, $message)
 {
+	/* headers are supposed to be \r\n terminated,
+	but the message body is to use \n line termination, according to the rfc */
+	$headers = "Content-type: text/plain; charset=iso-8859-1\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "X-Sender: <$from>\r\n";
+
+	// spoofing to avoid spam assassin filtering
+	$headers .= "X-Mailer: Internet Mail Service (5.5.2653.19)\r\n";
+	$headers .= "X-MimeOLE: Produced By Microsoft Exchange V6.0.6979.0\r\n";
+	$headers .= "User-Agent: Mozilla/5.001 (Windows; U; NT4.0; en-us) Gecko/25250101\r\n";
+
+	$headers .= "X-Priority: 3\r\n"; //1 Urgent Message, 3 Normal
+	$headers .= "X-MSMail-Priority: High\r\n"; // fix for hotmail spam filters? 
+	$headers .= "Return-Path: <$from>\r\n";
+	$headers .= "Reply-To: \"$fromname\" <$from>\r\n";
+	$headers .= "From: \"$fromname\" <$from>\r\n";
+
+	$recip = ($toname == "" ? "" : "\"$toname\" ") . $to;
+
 	global $sendMail;
 	if ($sendMail)
 	{
-		/* headers are supposed to be \r\n terminated,
-		but the message body is to use \n line termination, according to the rfc */
-		$headers = "Content-type: text/plain; charset=iso-8859-1\r\n";
-		$headers .= "MIME-Version: 1.0\r\n";
-		$headers .= "From: \"$fromname\" <$from>\r\n";
-		$headers .= "X-Sender: \"$fromname\" <$from>\r\n";
-	
-		// spoofing to avoid spam assassin filtering
-		$headers .= "X-Mailer: Internet Mail Service (5.5.2653.19)\r\n";
-		$headers .= "X-MimeOLE: Produced By Microsoft Exchange V6.0.6979.0\r\n";
-		$headers .= "User-Agent: Mozilla/5.001 (Windows; U; NT4.0; en-us) Gecko/25250101\r\n";
-	
-		$headers .= "X-Priority: 3\r\n"; //1 Urgent Message, 3 Normal
-		$headers .= "X-MSMail-Priority: High\r\n"; // fix for hotmail spam filters? 
-		$headers .= "Return-Path: \"$fromname\" <$from>\r\n";
-		$headers .= "Reply-To: \"$fromname\" <$from>\r\n";
-	
 		mail($recip, $subject, $message, $headers, "-f$to");
 	}
 }
@@ -72,11 +74,14 @@ if ($doit == 1)
 		$d = date("Y-m-d");
 		$id = preg_replace("/ /", "", $fields["name"]) . date("YmdHis");
 		$XML = <<<EOXML
+
+<!-- begin XML -->
 <review reviewName="{$fields["review_title"]}" date="$d" rating="{$fields["rating"]}" modelID="{$fields["model_id"]}" reviewID="$id">
 <text>{$fields["review"]}</text>
 <reviewer>{$fields["name"]}</reviewer>
 <reviewerURL>{$fields["website"]}</reviewerURL>
 </review>
+<!-- end XML -->
 
 EOXML;
 	}
@@ -101,8 +106,7 @@ EOXML;
 	}
 
 	// send to site admin, from customer
-	$recip = ($toname == "" ? "" : "\"$toname\" ") . "<" . $to . ">";
-	$footer = "\n\n".'$Id: models-mailform.php,v 1.35 2006/08/23 01:41:03 nickb Exp $';
+	$footer = "\n\n".'$Id: models-mailform.php,v 1.36 2006/08/23 01:49:55 nickb Exp $';
 	wmail("Modeling Corner", "emf@divbyzero.com", $fields["name"], $fields["email"], $subject, $messagePre . $message . $XML . $footer);
 
 	/* message */
