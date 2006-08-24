8ba7e7a501a8b7bd3aa3f91e503f753a3d9b33d5
nickb <nickb>
2006-08-24 23:42:42 +0000

    make download link dynamic to build server

--- _projectCommon.php
+++ _projectCommon.php
@@ -11,14 +11,15 @@
 	$Nav->setLinkList(null);
 
 	$rooturl = "http://www.eclipse.org";
-	$downurl = (preg_match("/^(download\.eclipse\.org|emf\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"], $regs) ? "http://$regs[1]" : "http://download.eclipse.org");
+	$downurl = (preg_match("/^(download\.eclipse\.org|emf\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"], $regs) ? "http://$regs[1]" : "http://www.eclipse.org");
+	$wnurl = (preg_match("/^(download\.eclipse\.org|emf\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"], $regs) ? "http://$regs[1]" : "http://download.eclipse.org");
 	$bugurl = "https://bugs.eclipse.org";
 
 	$Nav->addNavSeparator("EMF", "$rooturl/emf/emf.php");
 	$Nav->addCustomNav("SDO", "$rooturl/emf/sdo.php", "_self", 2);
 	$Nav->addCustomNav("XSD", "$rooturl/emf/xsd.php", "_self", 2);
 
-	$Nav->addNavSeparator("Downloads", "$rooturl/emf/downloads/");
+	$Nav->addNavSeparator("Downloads", "$downurl/emf/downloads/");
 	$Nav->addCustomNav("Installation", "$rooturl/emf/downloads/install.php", "_self", 2);
 	$Nav->addCustomNav("Update Manager", "$rooturl/tools/emf/updates/", "_self", 2);
 
@@ -26,7 +27,7 @@
 	$Nav->addCustomNav("Getting Started", "http://dev.eclipse.org/viewcvs/indextools.cgi/~checkout~/org.eclipse.emf/doc/org.eclipse.emf.doc/references/overview/EMF.html", "_self", 2);
 	$Nav->addCustomNav("FAQ", "$rooturl/emf/faq/faq.php", "_self", 2);
 	$Nav->addCustomNav("Release Notes", "$rooturl/emf/news/release-notes.php", "_self", 2);
-	$Nav->addCustomNav("What's New, CVS?", "$downurl/tools/emf/scripts/news-whatsnew-cvs.php?source=emf", "_self", 2);
+	$Nav->addCustomNav("What's New, CVS?", "$wnurl/tools/emf/scripts/news-whatsnew-cvs.php?source=emf", "_self", 2);
 
 	$Nav->addNavSeparator("Community", "http://wiki.eclipse.org/index.php/Modeling_Corner");
 	$Nav->addCustomNav("Newsgroup", "$rooturl/emf/newsgroup-mailing-list.php", "_self", 2);
