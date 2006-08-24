06f644ca8ff447080b74c46cb6ca3d3d841eac1a
nickb <nickb>
2006-08-24 23:25:02 +0000

    add more internal links

--- downloads/index.php
+++ downloads/index.php
@@ -222,6 +222,22 @@ if ($isEMFserver)
 		<li><a href="http://emf.torolab.ibm.com/emf/secure/promo.php">Promote</a></li>
 	</ul>
 </div>
+<div class="sideitem">
+	<h6>Info</h6>
+	<ul>
+		<li><a href="http://instawiki.webahead.ibm.com/pilot/wiki/Wiki.jsp?page=EMF&wiki=Rational_Modeling_Tools_Team">w3 Wiki</a></li>
+		<li><a href="https://bugs.eclipse.org/bugs/colchange.cgi?rememberedquery=product%3DEMF%2CXSD%26bug_status%3DASSIGNED%26order%3Dbugs.bug_id%26query_format%3Dadvanced&column_changeddate=on&column_bug_severity=on&column_priority=on&column_rep_platform=on&column_bug_status=on&column_product=on&column_component=on&column_version=on&column_target_milestone=on&column_short_short_desc=on&splitheader=0">Assigned Bugs</a></li>
+		<li><a href="http://emf.torolab.ibm.com/emf/downloads/downloads.php">Download Stats</a></li>
+	</ul>
+</div>
+<div class="sideitem">
+	<h6>Tests</h6>
+	<ul>
+		<li><a href="http://emf.torolab.ibm.com/tests/results-jdk14.php">1.4</a></li>
+		<li><a href="http://emf.torolab.ibm.com/tests/results-jdk50.php">5.0</a></li>
+		<li><a href="http://emf.torolab.ibm.com/tests/results.php">BVT, FVT, SVT</a></li>
+	</ul>
+</div>
 <?php
 }
 print "</div>\n";
