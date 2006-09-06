6840bd40d1a8cd72fad950a7a0e7c21567cacc6a
nickb <nickb>
2006-09-06 22:28:55 +0000

    [155444] publish searchcvs application using emft.eclipse.org as mysql server

---
+++ includes/db.php
@@ -0,0 +1,11 @@
+<?php
+$connect = mysql_connect("emft.eclipse.org", "modelingwww", "mjNDPzJ88v6xZRa4");
+mysql_select_db($db ? $db : "modeling") or die(mysql_error());
+
+function wmysql_query($sql)
+{
+	#print $sql . "\n";
+	$res = mysql_query($sql) or die("$sql\n" . mysql_error());
+	return $res;
+}
+?>
