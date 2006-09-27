3c2682692a41e776715d6fa005730c59040c9781
nickb <nickb>
2006-09-27 22:05:16 +0000

    extract rightnav into common shared file; remove todo

--- downloads/index.php
+++ downloads/index.php
@@ -218,35 +218,8 @@ print "<li><a href=\"?showAll=$showAll&amp;showMax=$showMax&amp;sortBy=$newsort\
 print "</ul>\n";
 print "</div>\n";
 
-if ($isEMFserver)
-{
-?>
-<div class="sideitem">
-	<h6>Actions</h6>
-	<ul>
-		<li><a href="http://emf.torolab.ibm.com/emf/build/">New Build</a></li>
-		<li><a href="http://emf.torolab.ibm.com/emf/build/patch.php">New Test</a></li>
-		<li><a href="http://emf.torolab.ibm.com/emf/build/promo.php">Promote</a></li>
-	</ul>
-</div>
-<div class="sideitem">
-	<h6>Info</h6>
-	<ul>
-		<li><a href="http://instawiki.webahead.ibm.com/pilot/wiki/Wiki.jsp?page=EMF&wiki=Rational_Modeling_Tools_Team">w3 Wiki</a></li>
-		<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=EMFT&amp;bug_status=ASSIGNED">Assigned Bugs</a></li>
-		<li><a href="http://emf.torolab.ibm.com/emf/downloads/downloads.php">Download Stats</a></li>
-	</ul>
-</div>
-<div class="sideitem">
-	<h6>Tests</h6>
-	<ul>
-		<li><a href="http://emf.torolab.ibm.com/tests/results-jdk14.php">JDK 1.4</a></li>
-		<li><a href="http://emf.torolab.ibm.com/tests/results-jdk50.php">JDK 5.0</a></li>
-		<li><a href="http://emf.torolab.ibm.com/tests/results.php">BVT, FVT, SVT</a></li>
-	</ul>
-</div>
-<?php
-}
+if ($isEMFserver) { include_once $pre."build/sideitems-common.php"; }
+
 print "</div>\n";
 
 $html = ob_get_contents();
@@ -1151,7 +1124,7 @@ function outputBuild($branch, $ID, $c)
 
 	if ($isEMFserver)
 	{
-	  // TODO: why is there no value for $summary?
+	  // $summary is passed in by reference, so no warning should appear here
 		$tests = getJDKTestResults("$jdk14testsPWD/", "$branch/$ID/", "jdk14", $summary) . "\n";
 		$summary .= ($summary ? "</span><span>" : "");
 		$tests .= getJDKTestResults("$jdk50testsPWD/", "$branch/$ID/", "jdk50", $summary) . "\n";
