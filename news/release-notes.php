473ab1a10f7edecec610cacdbd1943230466ac42
nickb <nickb>
2006-11-17 06:47:58 +0000

    [164914] add support for PHP5::XSL, PHP4::Sablotron, and error if PHP4::No XSLT; clean up warnings

--- news/release-notes.php
+++ news/release-notes.php
@@ -5,7 +5,7 @@ ob_start();
 
 // Process query string
 $params = array();
-if (preg_match("/^(\d\.\d(?:\.\d)?)$/", $_GET["version"], $regs))
+if (isset($_GET["version"]) && preg_match("/^(\d\.\d(?:\.\d)?)$/", $_GET["version"], $regs))
 {
 	$params["version"] = $regs[1];
 }
@@ -21,7 +21,7 @@ if ($params["version"] && is_file("release-notes" . $params["version"] . ".php")
 	exit;
 }
 
-if (preg_match("/^(" . join($projects, "|") . ")$/", $_GET["project"], $regs))
+if (isset($_GET["project"]) && preg_match("/^(" . join($projects, "|") . ")$/", $_GET["project"], $regs))
 {
 	$params["project"] = $regs[1];
 }
@@ -55,16 +55,56 @@ if (true)
 	if (is_file($XMLfile))
 	{
 		$XSLfile = "release-notes.xsl";
-		
-		$processor = xslt_create();
 		$fileBase = 'file://' . getcwd() . '/';
-		xslt_set_base($processor, $fileBase);
-		$result = xslt_process($processor, $fileBase . $XMLfile, $fileBase . $XSLfile, NULL, array(), $params);
+		$result = "";
 		
-		if (!$result)
+		if (phpversion() >= 5)
 		{
-			print "Trying to parse $XMLfile with $XSLfile...<br/>";
-			print "ERROR #" . xslt_errno($processor) . " : " . xslt_error($processor);
+	 		// PHP 5 w/ XSL
+	 	    $doc = new DOMDocument();
+		    $xsl = new XSLTProcessor();
+		 
+		    $doc->load($fileBase . $XSLfile);
+		    $xsl->importStyleSheet($doc);
+		 
+		    $doc->load($fileBase . $XMLfile);
+		    foreach ($params as $param => $paramVal)
+		    {
+			    $xsl->setParameter('', $param, $paramVal);
+		    }
+	 
+		    $result = $xsl->transformToXML($doc);
+			if (!$result)
+			{
+				
+				print '<div id="midcolumn"><div class="homeitem3col"><h3>An error has occurred!</h3>'."\n";
+				print "<ul><li><b>PHP5::XSL:</b> A problem occurred trying to parse $XMLfile with $XSLfile!</li></ul>";
+				print "</div></div>\n";	
+			}
+		}
+		else if (function_exists('xslt_create'))
+		{
+	 		// PHP 4 w/ Sablotron
+			$processor = xslt_create();
+			xslt_set_base($processor, $fileBase);
+			$result = xslt_process($processor, $fileBase . $XMLfile, $fileBase . $XSLfile, NULL, array(), $params);
+			if (!$result)
+			{
+				print '<div id="midcolumn"><div class="homeitem3col"><h3>An error has occurred!</h3>'."\n";
+				print "<ul><li><b>PHP4::Sablotron XSLT:</b> Trying to parse $XMLfile with $XSLfile: ";
+				print "ERROR #" . xslt_errno($processor) . " : " . xslt_error($processor);
+				print "</li></ul></div></div>\n";	
+			}
+		}
+		else
+		{
+			print '<div id="midcolumn"><div class="homeitem3col"><h3>An error has occurred!</h3>'."\n";
+			print "<ul><li><b>PHP4::No XSLT:</b> This page cannot be displayed. " .
+					"Try here instead: <a href=\"http://www.eclipse.org" . 
+				$_SERVER["SCRIPT_NAME"] . "?project=" . $params["project"] . "&amp;version=" . $params["version"] . "\">http://www.eclipse.org" . 
+				$_SERVER["SCRIPT_NAME"] . "?project=" . $params["project"] . "&amp;version=" . $params["version"] . "</a></li>" .
+					"</ul>\n";
+			print "</div></div>\n";	
 		}
 		
 		print $result;
