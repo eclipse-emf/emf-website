0baf08dfb3f2f59411d855d51969c66cf7b54401
nickb <nickb>
2006-12-21 21:31:38 +0000

    add header.php for internalUseOnly()

--- build/log-viewer.php
+++ build/log-viewer.php
@@ -1,4 +1,7 @@
 <?php 
+# TODO: remove this when migrated to /modeling/emf
+require_once($_SERVER['DOCUMENT_ROOT'] . "/emf/includes/header.php"); 
+
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
 
 internalUseOnly(); 
