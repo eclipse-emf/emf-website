788eb9bebc4e8995d4e3107539996247bd6f00bf
nickb <nickb>
2007-03-30 19:41:37 +0000

    [178148] merged from modeling

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,7 +1,23 @@
 <?php
-$debug = (isset ($_GET["debug"]) && preg_match("/^\d+$/", $_GET["debug"]) ? $_GET["debug"] : -1);
 
-if (isset ($_GET["skin"]) && preg_match("/^(Blue|EclipsStandard|Industrial|Lazarus|Miasma|OldStyle|Phoenix|PlainText)$/", $_GET["skin"], $regs))
+$Nav->setLinkList(null);
+
+$PR = "emf";
+
+$isEMFserver = (preg_match("/^emf(?:\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"]));
+$isBuildServer = (preg_match("/^(emft|build)\.eclipse\.org$/", $_SERVER["SERVER_NAME"])) || $isEMFserver;
+$isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
+$isWWWserver = (preg_match("/^(?:www.|)eclipse.org$/", $_SERVER["SERVER_NAME"]));
+$isEclipseCluster = (preg_match("/^(?:www.||download.|download1.|build.)eclipse.org$/", $_SERVER["SERVER_NAME"]));
+$debug = (isset ($_GET["debug"]) && preg_match("/^\d+$/", $_GET["debug"]) ? $_GET["debug"] : -1);
+$writableRoot = ($isBuildServer ? $_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/" : "/home/data/httpd/writable/www.eclipse.org/");
+$writableBuildRoot = $isBuildDotEclipseServer ? "/opt/public/modeling" : "/home/www-data";
+
+$rooturl = "http://" . $_SERVER["HTTP_HOST"] . "/$PR";
+$downurl = ($isBuildServer ? "" : "http://www.eclipse.org");
+$bugurl = "https://bugs.eclipse.org";
+
+if (isset ($_GET["skin"]) && preg_match("/^(Blue|EclipseStandard|Industrial|Lazarus|Miasma|Modern|OldStyle|Phoenix|PhoenixTest|PlainText)$/", $_GET["skin"], $regs))
 {
 	$theme = $regs[1];
 }
@@ -10,53 +26,56 @@ else
 	$theme = "Phoenix";
 }
 
-$Nav->setLinkList(null);
+/* projects/components in cvs */
+/* "proj" => "cvsname" */
+$cvsprojs = array(
+	"emf" => "org.eclipse.emf"
+);
 
-$isEMFserver = (preg_match("/emf(?:\.torolab\.ibm\.com)?/", $_SERVER["SERVER_NAME"]));
-$isWWWserver = (preg_match("/^(?:www.|)eclipse.org$/", $_SERVER["SERVER_NAME"]));
-$isEclipseCluster = (preg_match("/^(?:www.||download.|download1.)eclipse.org$/", $_SERVER["SERVER_NAME"]));
-
-$rooturl = "http://www.eclipse.org/emf";
-$downurl = (preg_match("/^(emf\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"], $regs) ? "http://$regs[1]" : "http://www.eclipse.org");
-$bugurl = "https://bugs.eclipse.org";
+/* sub-projects/components in cvs for projects/components above (if any) */
+/* "cvsname" => array("shortname" => "cvsname") */
+$cvscoms = array();
 
 $projects = array (
-	"EMF, SDO & XSD" => "",
-	"EMF &amp; SDO" => "emf",
-	"XSD" => "xsd"
+	"EMF &amp; SDO" => "emf"
 );
 
-$cvsprojs = array(
-	"emf" => "org.eclipse.emf",
-	"xsd" => "org.eclipse.xsd"
-);
-
-$cvscoms = array();
+$extraprojects = array(); //projects with only downloads, no info yet, "prettyname" => "directory"
+$nodownloads = array(); //projects with only information, no downloads, or no builds available yet, "projectkey"
+$nonewsgroup = array (); //projects without newsgroup
+$nomailinglist = array (); //projects without mailinglist
+$incubating = array(); // projects which are incubating - EMF will never have incubating components!
 
 $nomenclature = "Component"; //are we dealing with "components" or "projects"?
 
 $regs = null;
 $proj = (isset ($_GET["project"]) && preg_match("/^(" . join("|", $projects) . ")$/", $_GET["project"], $regs) ? $regs[1] : "");
-$PR = "emf";
+
+$buildtypes = array(
+	"R" => "Release",
+	"S" => "Stable",
+	"I" => "Integration",
+	"M" => "Maintenance",
+	"N" => "Nightly"
+);
 
 // this isn't quite the same as EMFT or MDT... yet
-$Nav->addNavSeparator("EMF", "$rooturl/emf.php");
-$Nav->addCustomNav("SDO", "$rooturl/sdo.php", "_self", 2);
-$Nav->addCustomNav("XSD", "$rooturl/xsd.php", "_self", 2);
+$Nav->addNavSeparator("EMF", "$rooturl/?project=emf");
+$Nav->addCustomNav("SDO", "$rooturl/?project=sdo", "_self", 2);
 
-$Nav->addNavSeparator("Downloads", "$downurl/emf/downloads/");
+$Nav->addNavSeparator("Downloads", "$downurl/$PR/downloads/?project=$proj");
 $Nav->addCustomNav("Installation", "$rooturl/downloads/install.php", "_self", 2);
 $Nav->addCustomNav("Update Manager", "$rooturl/updates/", "_self", 2);
 
 $Nav->addNavSeparator("Documentation", "$rooturl/docs/");
 $Nav->addCustomNav("Getting Started", "http://dev.eclipse.org/viewcvs/indextools.cgi/*checkout*/org.eclipse.emf/doc/org.eclipse.emf.doc/references/overview/EMF.html", "_self", 2);
-$Nav->addCustomNav("FAQ", "$rooturl/faq/faq.php", "_self", 2);
+$Nav->addCustomNav("FAQ", "$rooturl/faq/", "_self", 2);
 $Nav->addCustomNav("Release Notes", "http://www.eclipse.org/modeling/emf/news/relnotes.php?project=emf&version=HEAD", "_self", 2);
 $Nav->addCustomNav("Search CVS", "$rooturl/searchcvs.php?q=project%3A+org.eclipse.emf+days%3A+7", "_self", 2);
 
 $Nav->addNavSeparator("Community", "http://wiki.eclipse.org/index.php/Modeling_Corner");
 $Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/Eclipse_Modeling_Framework", "_self", 2);
-$Nav->addCustomNav("Newsgroup", "$rooturl/newsgroup-mailing-list.php", "_self", 2);
+$Nav->addCustomNav("Newsgroups", "$rooturl/newsgroup-mailing-list.php", "_self", 2);
 $Nav->addCustomNav("Modeling Corner", "http://wiki.eclipse.org/index.php/Modeling_Corner", "_self", 2);
 $collist = "%26query_format%3Dadvanced&amp;column_changeddate=on&amp;column_bug_severity=on&amp;column_priority=on&amp;column_rep_platform=on&amp;column_bug_status=on&amp;column_product=on&amp;column_component=on&amp;column_version=on&amp;column_target_milestone=on&amp;column_short_short_desc=on&amp;splitheader=0";
 $Nav->addCustomNav("Open Bugs", "$bugurl/bugs/colchange.cgi?rememberedquery=product%3DEMF%2CXSD%26bug_status%3DNEW%26bug_status%3DASSIGNED%26bug_status%3DREOPENED%26order%3Dbugs.bug_status%2Cbugs.target_milestone%2Cbugs.bug_id" . $collist, "_self", 2);
