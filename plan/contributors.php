44720e83fcafade4ee53208048143e3ffa8139bd
nickb <nickb>
2007-06-07 19:07:16 +0000

    revised for plaintext output

--- plan/contributors.php
+++ plan/contributors.php
@@ -1,49 +1,28 @@
 <?php
-   # Props to Denis Roy (webmaster@eclipse.org) for the basecode from which this script grew.
-   # Logic, DB and Presentation lumped here for simplicity.
-   # Please avoid using aggregate functions (COUNT, SUM, MAX, MIN, etc) on busy web pages.
-   # For bugzilla this is not critical as the tables are small (<10,000,000 records)
-   # but imagine if every project displays a COUNT(*) for their project's bugs right on the front page!
-   # ** NOTE ** You need to tell the WebMaster from which URL you are loading this class from, 
-   # otherwise the connect() will fail.
+# Props to Denis Roy (webmaster@eclipse.org) for the basecode from which this script grew.
+# Logic, DB and Presentation lumped here for simplicity.
+# Please avoid using aggregate functions (COUNT, SUM, MAX, MIN, etc) on busy web pages.
+# For bugzilla this is not critical as the tables are small (<10,000,000 records)
+# but imagine if every project displays a COUNT(*) for their project's bugs right on the front page!
+# ** NOTE ** You need to tell the WebMaster from which URL you are loading this class from, 
+# otherwise the connect() will fail.
    
-   # Load up the classfile
-   require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
+# Load up the classfile
+require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_bugs_ro.class.php";
 
-   header("Content-Type: text/html");
+header("Content-Type: text/plain");
 
-   $bug = $_GET["bug"]; if (!$bug) { $bug="185971"; }
-   $query = stripslashes($_POST["query"]);
-   echo '
-<html>
-<head></head>
-<body>
-<table><form method=post><tr valign="top"><td align="left">
-   <pre style="font-size:12px">Query:<br><i style="font-size:11px">separate multiple queries with semi-colon (";")
-  -&gt; <a href="https://dev.eclipse.org/committers/committertools/dbo_bugs_schema.php">database schema</a> (fields, tables, constraints)</i></pre>
-   <textarea style="font-size:12px" name=query rows=40 cols=60>'.$query.'</textarea><br/>
-   <input type=submit name="Submit" style="font-size:12px">
-   <pre style="font-size:12px;color:navy">
-';
-   echo <<<EOHTML
+$bugs = $_GET["bugs"]; 
+if (!$bugs) 
+{ 
+	echo "Enter a list of bugs to return csv data, eg:\n?bugs=147594,149770,161744,165770,166967,166112,170204,156783,170223,136881,179004,185971\n\n"; 
+	exit; 
+}
+
+# Component,Bug #,Contributor,Size,Committer
+$query = "
 SELECT DISTINCT
-  BUG.bug_id, PROF.realname, PROF.login_name, TXT.bug_when
-FROM 
-  bugs as BUG, 
-  profiles as PROF, 
-  longdescs as TXT 
-WHERE 
-  TXT.who = PROF.userid AND 
-  BUG.bug_id = TXT.bug_id AND 
-  PROF.realname NOT IN ('Nick Boldt','Marcelo Paternostro', 
-	'Dave Steinberg', 'Ed Merks') AND
-  BUG.bug_id IN (147594, 149770, 161744, 165770, 166967, 
-  	166112, 170204, 156783, 170223, 136881, 179004, 185971)
-ORDER BY
-  TXT.bug_when
-ASC;
-SELECT DISTINCT
-  BUG.bug_id, PROF.realname, TXT.bug_when, TXT.thetext
+  BUG.bug_id, PROF.login_name, TXT.thetext
 FROM 
   bugs as BUG, 
   profiles as PROF, 
@@ -52,73 +31,77 @@ WHERE
   TXT.who = PROF.userid AND 
   BUG.bug_id = TXT.bug_id AND 
   TXT.thetext like '%[contrib%]%' AND
-  BUG.bug_id IN (147594, 149770, 161744, 165770, 166967, 
-  166112, 170204, 156783, 170223, 136881, 179004, 185971)
+  BUG.bug_id IN (".$bugs.")
 ORDER BY
   TXT.bug_when
-ASC
+ASC";
+   
+$data = array();
 
-</pre>
-</td><td>&nbsp;&nbsp;</td>
-<td>
-EOHTML;
+# Connect to database
+$dbc = new DBConnectionBugs();
+$dbh = $dbc->connect();
 
-	if (false!==strpos($query,";")) {
-		$queries = explode(";",$query);
-	} else {
-		$queries = array($query);
+$rs = mysql_query($query, $dbh);
+if(mysql_errno($dbh) > 0) 
+{
+	echo "Error ".mysql_errno($dbh). ": ".mysql_error($dbh)."\n\n";
+	# Mysql disconnects automatically, but I like my disconnects to be explicit.
+	$dbc->disconnect();
+	$dbh = null;
+	$dbc = null;
+} 
+else 
+{
+	while($myrow = mysql_fetch_row($rs)) 
+	{
+		$contrib_email = preg_replace("/.*(\\[contrib email=\".+\"\/\\]).*/","$1",str_replace("\n"," ",$myrow[2]));
+		$data["b".$myrow[0]] = ",".$myrow[0].",%%CONTRIB_EMAIL=".$contrib_email."%%,,".preg_replace("/().+)\@.+/","$1",$myrow[1]);
+		$contributors[$contrib_email] = $contrib_email; 
 	}
-	foreach ($queries as $i => $query) { 
-		if ($query) { 
-			echo "<pre>Results".(sizeof($queries)>1?"[".($i+1)."]":"").":</pre>\n";
-			echo "<pre style=\"color:blue\">";
-			# Connect to database
-			$dbc  = new DBConnectionBugs();
-			$dbh  = $dbc->connect();
-
-			$rs   = mysql_query($query, $dbh);
-			
-			if(mysql_errno($dbh) > 0) {
-				echo "There was an error processing the query.</pre>\n".
-				# Mysql disconnects automatically, but I like my disconnects to be explicit.
-				$dbc->disconnect();
-				$dbh = null;
-				$dbc = null;
-			} else {
-				$header=false;
-				while($myrow = mysql_fetch_assoc($rs)) {
-					if (!$header)
-					{
-						foreach ($myrow as $k => $v) {
-							echo "$k\t";
-						}
-						$header=true;
-						echo "\n";
-					}
-					foreach ($myrow as $k => $v) {
-						if ($k == "bug_id") { 
-							echo "<a style=\"color:purple\" href=\"https://bugs.eclipse.org/bugs/show_bug.cgi?id=$v\" target=\"_bug\">$v</a>\t";
-						} else if ($k == "thetext") {
-							echo preg_replace("/.*(\\[contrib.+\\]).*/","$1",str_replace("\n"," ",$v))."\t";
-						} else {
-							echo "$v\t";
-						}
-					}
-					echo "\n";
-				}
-				echo "</pre>";
-				$dbc->disconnect();
-				$rs  = null;
-				$dbh = null;
-				$dbc = null;
-			}
+	$myrow = null;
+	$query = "
+	SELECT DISTINCT
+	  PROF.login_name, PROF.realname 
+	FROM 
+	  profiles as PROF 
+	WHERE 
+	  PROF.login_name IN ('".join($contributors,"', '")."')";
+	$rs = mysql_query($query, $dbh);
+	if(mysql_errno($dbh) > 0) 
+	{
+		echo "Error ".mysql_errno($dbh). ": ".mysql_error($dbh)."\n\n";
+		# Mysql disconnects automatically, but I like my disconnects to be explicit.
+		$dbc->disconnect();
+		$dbh = null;
+		$dbc = null;
+	}
+	else 
+	{
+		$data = array();
+		while($myrow = mysql_fetch_row($rs)) 
+		{
+			$contributors[$myrow[0]] = $myrow[1];
 		}
 	}
+	$dbc->disconnect();
+	$rs  = null;
+	$dbh = null;
+	$dbc = null;
+	
+	foreach ($data as $b => $line)
+	{
+		$m = null;
+		preg_match("/%%CONTRIB_EMAIL=(.+)%%/",$line,$m);
+		if (array_key_exists($m[1],$contributors) && isset($contributors[$m[1]]))
+		{
+			$data[$b] = preg_replace("/%%CONTRIB_EMAIL=.+%%/",$contributors[$m[1]],$line);
+		}
+	}
+}
 
-	echo '
-</td></tr></form></table>
-</body>
-</html>
-';
-
+foreach ($data as $b => $line)
+{
+	print $line."\n";
+}
 ?>
